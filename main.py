852b857a1fd64dbbc7e61fe9a43407f74978422d
tjperr <thomas_perrett@outlook.com>
2023-01-10 15:25:03 +0000

    add complexity

--- main.py
+++ main.py
@@ -19,33 +19,54 @@ class Meal:
     meat: string
     freq: int
     n_meals: int
+    cuisine: string
+    complexity: int
 
 
 CARB_LIMIT = 2  # max acceptable same carb in a week
 MEAT_LIMIT = 2  # max acceptable same meat in a week
-START_DT = datetime(2022, 11, 21, 19, 0, 0)  # The first meal to be planned
-N_MEALS = 365 # number of meals to plan
+COMPLEXITY_LIMIT = 14
+START_DT = datetime(2023, 1, 17, 19, 0, 0)  # The first meal to be planned
+N_MEALS = 3*30  # number of meals to plan
 
 
 def plan(meals: List[Meal], days: int):
     """
     make a meal plan for `days` days from the meals in `meals`
     """
-
     plan = []
     while len(plan) < days:
 
+        print(len(plan) / days)
+
+        # for m in plan[-7:]:
+        #    print(m)
+        # print("----------------")
+
         candidate = random.choice(meals)
 
+        # print(candidate)
+        # print("----------------")
+
         if candidate in plan[-candidate.freq :]:  # Frequency limit
+            # print("frequency limit")
             pass
         elif (
             sum([m.carb == candidate.carb for m in plan[-7:]]) > CARB_LIMIT
         ):  # consecutive carb limit
+            # print('carb limit')
             pass
         elif (
             sum([m.meat == candidate.meat for m in plan[-7:]]) > MEAT_LIMIT
         ):  # consecutive meat limit
+            # print('meat limit')
+            pass
+        elif (
+            sum([m.complexity for m in plan[-7:]])
+            + (candidate.n_meals * candidate.complexity)
+            > COMPLEXITY_LIMIT
+        ):
+            # print('complexity limit')
             pass
         else:
             plan += candidate.n_meals * [candidate]
